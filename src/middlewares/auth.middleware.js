bbe238e16c8f7535e12e8c2cfe19af8630553571
FizaShakil <fizashakil25@gmail.com>
2024-11-01 22:08:41 +0500

    Adding the missed jwt import

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import {ApiError} from '../utils/ApiError.js'
 import { User } from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
+import jwt from 'jsonwebtoken'
 
 const verifyJWT = asyncHandler(async(req,res,next)=>{
     try {
